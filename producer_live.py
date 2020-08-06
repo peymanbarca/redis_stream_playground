39036b24ab5c53e7d60deb8bc43d868396f13c01
vacek <p.yazdanian74@gmail.com>
2020-08-06 18:35:49 +0430

    producer run for ever in loop asyncio added

--- producer_live.py
+++ producer_live.py
@@ -3,17 +3,39 @@ import time
 import aioredis
 import random
 import uvloop
+import pyfirmata
+import time
+
+
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-
 loop = asyncio.get_event_loop()
+
+
+#board = pyfirmata.Arduino('/dev/ttyACM0')
+async def read_temperature_and_humidity(board):
+
+    sw = board.digital[10].read()
+    if sw is True:
+        board.digital[13].write(1)
+    else:
+        board.digital[13].write(0)
+
+    temperature=board.digital[15].read()
+    humidity=board.digital[16].read()
+
+    return temperature,humidity
 
 
 async def add_message_with_sleep(redis, loop, stream):
 
     for _ in range(10):
+
+        #temperature, humidity = await read_temperature_and_humidity(board=board)
+
         temperature = str(random.randrange(20, 30))
         humidity = str(random.randrange(10, 20))
+
         fields = {'temperature': temperature.encode('utf-8'),
                   'humidity': humidity.encode('utf-8')}
         print(_,fields)
